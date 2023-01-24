a6885a770e71907ede439fc24e947ae955d7b96a
jigar <jigar@storat.com>
2023-01-24 16:11:47 +0530

    changes

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,6 +1,19 @@
 
-function AnimalShow(){
-    return <div>Cow!</div>;
+import cat from './svg/cat.svg';
+import bird from './svg/bird.svg';
+import cow from './svg/cow.svg';
+import dog from './svg/dog.svg';
+import gator from './svg/gator.svg';
+import horse from './svg/horse.svg';
+
+const svgImage = {
+    cat,bird,cow,dog,gator,horse
+};
+
+function AnimalShow({type}){
+    return <div>
+        <img src={svgImage[type]} alt='animal' width="100px"></img>
+    </div>;
 }
 
 export default AnimalShow;
--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import {useState} from 'react';
+import { useState } from "react";
+import  AnimalShow  from './AnimalShow';
 
-function App(){
-    const [count, setCount ] = useState(0);
-
-    const handleClick = () => {
-        console.log("button is clicked");
-        setCount(count+1);
-    };
-
-    return <div>
-        <button onClick={handleClick}>Add Animal</button>
-        Animal to show : {count}
-    </div>
+function getRandomAnimal() {
+  const animals = ["bird", "cow", "horse", "cat", "dog", "gator"];
+  return animals[Math.floor(Math.random() * animals.length)];
 }
 
+
+function App() {
+  const [animals, setAnimals] = useState([]);
+  const randomAnimals = animals.map((animal,index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
+
+  const handleClick = () => {
+    setAnimals([...animals,getRandomAnimal()]);
+  };
+  return (
+    <div>
+      <button onClick={handleClick}>Add Animal</button>
+      {randomAnimals}
+    </div>
+  );
+}
 
 export default App;
